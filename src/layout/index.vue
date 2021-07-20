3d71f7b7f45c685c13d8e0ce65ca82b15858e38f
SeaChan <1341914555@qq.com>
2021-07-20 21:54:37 +0800

    layout 和嵌套路由处理

---
+++ src/layout/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="layout">
+    布局组件
+    <h2>头部</h2>
+    <h2>侧边栏</h2>
+
+<!--    子路由出口-->
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LayoutIndex'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
