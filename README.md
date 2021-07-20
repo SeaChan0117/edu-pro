0869cca4b35ad5838ed0d1f192d96084dda949fe
SeaChan <1341914555@qq.com>
2021-07-20 16:48:03 +0800

    全局样式处理，请求模块封装初始化

--- README.md
+++ README.md
@@ -22,3 +22,10 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### some problems
+
+#### 处理全局样式变量时
+报错：```Using / for division is deprecated and will be removed in Dart Sass 2.0.0.```
+
+解决方法： 需要在package.json文件里修改"sass": "~1.32.6"即可
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,43 @@
 <template>
   <div id="app">
     <h1>拉钩教育</h1>
-<!--    跟路由出口-->
+    <!--    跟路由出口-->
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+
+    <p class="text">hello world</p>
     <router-view/>
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+import request from '@/utils/request'
+
+request({
+  method: 'GET',
+  url: '/boss/v2/api-docs?group=edu-boss-boot'
+}).then(_ => {
+  console.log(_)
+})
+
+export default Vue.extend({
+  name: 'App',
+  data () {
+    return {
+      foo: {}
+    }
+  }
+})
+</script>
 <style lang="scss" scoped>
+.text {
+  color: $success-color;
+}
 </style>
